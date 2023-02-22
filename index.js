3c2144e9a5450b9843d33b8e3c1be39db98431b8
Zachary Crespin <Zachary.lights@gmail.com>
2023-02-22 15:17:29 -0800

    fix headers

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
   fs.readFile('quotes.json', function(err, buf) {
     var qarr = JSON.parse(buf);
     var random = Math.floor(Math.random() * (qarr.length - 1));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.send(qarr[random]);
   });
 });
