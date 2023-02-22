a0b9c20236df3cfa8131544ba9582caa641c18c3
Zachary Crespin <Zachary.lights@gmail.com>
2023-02-22 15:09:37 -0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # quote-api
 
-[quote.zacharyc.site](https://quote.zacharyc.site/)
+## View the website
+[quotes.zacharyc.site](http://quotes.zacharyc.site/)
+## Use the API
+[api.quotes.zacharyc.site](https://api.quotes.zacharyc.site/)
+
+## Add your quote
+Add your quote to the end of `quotes.db` with the format:
+```
+{
+  "quote": "Quiet people have the loudest minds.",
+  "author": "Stephen Hawking"
+}
+```
--- index.js
+++ index.js
@@ -8,27 +8,10 @@ app.get('/', (req, res) => {
   fs.readFile('quotes.json', function(err, buf) {
     var qarr = JSON.parse(buf);
     var random = Math.floor(Math.random() * (qarr.length - 1));
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET'
-    );
-
-    // Request headers you wish to allow
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-Requested-With,content-type'
-    );
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
     res.send(qarr[random]);
   });
 });
 
-// Httpserver Port Number 3000.
 app.listen(process.env.PORT || 3000, function() {
   console.log(
     'Express server listening on port %d in %s mode',
@@ -36,6 +19,3 @@ app.listen(process.env.PORT || 3000, function() {
     app.settings.env
   );
 });
-
-// Set The File Type To App As EJS.
-app.set('view engine', 'ejs');
