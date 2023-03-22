e5e6b9f369737bd3eb4cb6111b5d30b21e3bbc26
Zachary Crespin <Zachary.lights@gmail.com>
2023-03-22 11:54:53 -0700

    simple updates

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 
-var fs = require('fs');
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-  fs.readFile('quotes.json', function(err, buf) {
-    var qarr = JSON.parse(buf);
-    var random = Math.floor(Math.random() * (qarr.length - 1));
+  fs.readFile('quotes.json', function(err, quotes) {
+    quotes = JSON.parse(quotes);
+    let random = Math.floor(Math.random() * (quotes.length - 1));
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.send(qarr[random]);
+    res.send(quotes[random]);
   });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    'listening on port http://localhost:%d',
+    this.address().port
+  );
+});
